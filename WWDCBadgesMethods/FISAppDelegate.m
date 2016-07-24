fe8941e9e7cce078bf85bda76b429b6f37fcdeaa
Brian Mesa <bmesa620@gmail.com>
2016-07-24 00:51:30 -0400

    done

--- WWDCBadgesMethods/FISAppDelegate.m
+++ WWDCBadgesMethods/FISAppDelegate.m
@@ -15,10 +15,20 @@
     return YES;
 }
 
-/*
- 
- * Define your methods between application:didFinishLaunchingWithOptions and the @end marker
- 
- */
+- (NSString *)badgeForSpeaker:(NSString *)speaker{
+    NSString *result = [NSString stringWithFormat:@"Hello, my name is %@.", speaker];
+    return result;
+}
+- (NSArray *)badgesForSpeakers:(NSArray *)speakers{
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    for (NSUInteger i = 0; i < [speakers count]; i++) {
+        NSString *hello = [NSString stringWithFormat:@"Hello, my name is %@.", speakers[i]];
+        [result addObject:hello];
+    }
+    return result;
+}
+- (NSArray *)greetingsAndRoomAssignmentsForSpeakers:(NSArray *)speakers{
+    return nil;
+}
 
 @end
